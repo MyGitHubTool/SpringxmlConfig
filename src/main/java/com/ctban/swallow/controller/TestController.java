46562b274488d080e7262fddf817b4dcdc236749
guojie <2874267468@qq.com>
2018-07-04 12:59:01 +0800

    完善demo

--- src/main/java/com/ctban/swallow/controller/TestController.java
+++ src/main/java/com/ctban/swallow/controller/TestController.java
@@ -5,10 +5,7 @@ import com.ctban.swallow.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,9 +24,9 @@ public class TestController {
 
     @RequestMapping("/findUserInfoByUserId")
     @ResponseBody
-    public List<UserDTO> findUserInfoByUserId(@RequestParam Long id) {
+    public List<UserDTO> findUserInfoByUserId(@RequestBody UserDTO userDTO) {
         List<UserDTO> list = new ArrayList<>();
-        list.add(userService.findUserInfoId(id));
+        list.add(userService.findUserInfoId(userDTO.getId()));
         return list;
     }
 
