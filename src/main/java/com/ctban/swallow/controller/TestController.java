e8459d104920f801f271c0adc2b33ebeb4cbb657
guojie <2874267468@qq.com>
2018-07-04 12:02:00 +0800

    提交目录结构

--- src/main/java/com/ctban/swallow/controller/TestController.java
+++ src/main/java/com/ctban/swallow/controller/TestController.java
@@ -1,7 +1,42 @@
 package com.ctban.swallow.controller;
 
+import com.ctban.swallow.dto.UserDTO;
+import com.ctban.swallow.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author GuoJie 2018/7/4 9:04
  */
+@Controller
+@RequestMapping("test")
 public class TestController {
+    @Autowired
+    Environment ev;
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("/findUserInfoByUserId")
+    @ResponseBody
+    public List<UserDTO> findUserInfoByUserId(@RequestParam Long id) {
+        List<UserDTO> list = new ArrayList<>();
+        list.add(userService.findUserInfoId(id));
+        return list;
+    }
+
+    @RequestMapping("testJsp")
+    public String testJsp() {
+
+        return "hello";
+    }
 }
+
